f2d3b304f22e5ed813d796ee53ed3142f9987e49
Ukuma012 <Ukuma012@gmail.com>
2023-06-08 12:00:00 +0900

    packet received

--- main.c
+++ main.c
@@ -1,14 +1,32 @@
 #include <stdio.h>
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <stdlib.h>
 #include <netinet/in.h>
+#include <linux/if_ether.h>
 
 int main(int argc, char *argv[]) {
   int socketfd;
-  if((socketfd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) {
+  if((socketfd = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL))) < 0) {
     perror("socket failed\n");
     exit(1);
+  }
+
+  unsigned char buffer[1500];
+  while(1) {
+    ssize_t n;
+    if((n = recv(socketfd, buffer, sizeof(buffer), 0)) < 0) {
+      perror("recv failed\n");
+      exit(1);
+    }
+    if(n != 0) {
+      printf("Received %lu bytes:", n);
+      for(int i = 0; i < n; i++) {
+        printf("%02X", buffer[i]);
+      }
+      printf("\n");
+    }
   }
   
   close(socketfd);
