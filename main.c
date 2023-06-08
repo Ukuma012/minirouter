5d12e57592fb991eb2dfe11eff8eb6ca43b8c44b
Ukuma012 <Ukuma012@gmail.com>
2023-06-08 11:44:08 +0900

    socket

--- main.c
+++ main.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <netinet/in.h>
 
 int main(int argc, char *argv[]) {
-  printf("%s\n", "Hello World");
+  int socketfd;
+  if((socketfd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) {
+    perror("socket failed\n");
+    exit(1);
+  }
+  
+  close(socketfd);
   return 0;
 }
