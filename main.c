451825493b6b59deb77810f48eeb3c84dc7d8e6c
Ukuma012 <Ukuma012@gmail.com>
2023-06-10 14:14:52 +0900

    ifaddrs

--- main.c
+++ main.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include <ifaddrs.h>
 #include <sys/socket.h>
 #include <stdlib.h>
 #include <netinet/in.h>
 #include <linux/if_ether.h>
 
 int main(int argc, char *argv[]) {
+  struct ifaddrs *ifaddrs;
+  getifaddrs(&ifaddrs);
+  
   int socketfd;
   if((socketfd = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL))) < 0) {
     perror("socket failed\n");
