404638fc985c413b5f59fa030178af1822e22eb4
Ukuma012 <Ukuma012@gmail.com>
2023-06-11 17:00:18 +0900

    delete a comment

--- main.c
+++ main.c
@@ -7,9 +7,8 @@
 #include <netinet/in.h>
 #include <linux/if_ether.h>
 
-// TODO インターフェースを区別する
-
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
   struct ifaddrs *ifaddrs;
   getifaddrs(&ifaddrs);
 
@@ -21,29 +20,34 @@ int main(int argc, char *argv[]) {
   //   printf("Address Family: %d\n", tmp->ifa_addr->sa_family);
   //   printf("-----------------\n");
   // }
-  
+
   int socketfd;
-  if((socketfd = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL))) < 0) {
+  if ((socketfd = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL))) < 0)
+  {
     perror("socket failed\n");
     exit(1);
   }
 
   unsigned char buffer[1500];
-  while(1) {
+  while (1)
+  {
     ssize_t n;
-    if((n = recv(socketfd, buffer, sizeof(buffer), 0)) < 0) {
+    if ((n = recv(socketfd, buffer, sizeof(buffer), 0)) < 0)
+    {
       perror("recv failed\n");
       exit(1);
     }
-    if(n != 0) {
+    if (n != 0)
+    {
       printf("Received %lu bytes:", n);
-      for(int i = 0; i < n; i++) {
+      for (int i = 0; i < n; i++)
+      {
         printf("%02X", buffer[i]);
       }
       printf("\n");
     }
   }
-  
+
   close(socketfd);
   freeifaddrs(ifaddrs);
   return 0;
