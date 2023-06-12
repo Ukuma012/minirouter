b9fe180e8bacb8f7f1966ffa60de153575e1d760
Ukuma012 <Ukuma012@gmail.com>
2023-06-12 22:41:41 +0900

    header

--- ethernet.c
+++ ethernet.c
@@ -1,6 +1,9 @@
 #include "ethernet.h"
 #include <stdio.h>
+#include <stdint.h>
 
-void ether_hello() {
-    printf("%s\n", "Hello World from ethernet");
-}
+struct ethernet_header {
+    uint8_t dest_mac_addr[6];
+    uint8_t source_mac_addr[6];
+    uint16_t ethertype;
+} __attribute__((__packed__));
