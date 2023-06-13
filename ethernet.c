628fe6dc5ba9ef72f64f6eee7e902e04e9025fea
Ukuma012 <Ukuma012@gmail.com>
2023-06-13 17:53:06 +0900

    ntohs

--- ethernet.c
+++ ethernet.c
@@ -1,6 +1,7 @@
 #include "ethernet.h"
 #include <stdio.h>
 #include <stdint.h>
+#include <arpa/inet.h>
 
 struct ethernet_header {
     uint8_t dest_mac_addr[6];
@@ -15,5 +16,5 @@ void ether_dump(unsigned char *buffer) {
     printf("Ethernet Header\n");
     printf("Destination MAC: %02X:%02X:%02X:%02X:%02X:%02X\n", ether_header->dest_mac_addr[0], ether_header->dest_mac_addr[1], ether_header->dest_mac_addr[2], ether_header->dest_mac_addr[3], ether_header->dest_mac_addr[4], ether_header->dest_mac_addr[5]);
     printf("Source MAC: %02X:%02X:%02X:%02X:%02X:%02X\n", ether_header->source_mac_addr[0], ether_header->source_mac_addr[1], ether_header->source_mac_addr[2], ether_header->source_mac_addr[3], ether_header->source_mac_addr[4], ether_header->source_mac_addr[5]);
-    printf("Type: %04X\n", ether_header->ethertype);
+    printf("Type: %04X\n", ntohs(ether_header->ethertype));
 }
