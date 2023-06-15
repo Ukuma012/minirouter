6ddc7c62e797368305919e6fead2926b77e3a536
Ukuma012 <Ukuma012@gmail.com>
2023-06-15 13:51:40 +0900

    hardware type

--- arp.c
+++ arp.c
@@ -1,4 +1,7 @@
+#include <stdio.h>
 #include <stdint.h>
+#include <arpa/inet.h>
+#include "arp.h"
 
 struct arp_header {
    uint16_t hardware_type;
@@ -16,7 +19,7 @@ void arp_dump(unsigned char *buffer) {
     struct arp_header* arp_header;
     arp_header = (struct arp_header *)buffer;
 
-    printf("%04X", ntoh(arp_header->hardware_type));
+    printf("%04X\n", ntohs(arp_header->hardware_type));
 
     return;
 }
