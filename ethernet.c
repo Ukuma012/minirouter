5ae7f0043ef7fcc0a4bfbbd8355e9d904b4fcfba
Ukuma012 <Ukuma012@gmail.com>
2023-06-15 12:02:09 +0900

    modified

--- ethernet.c
+++ ethernet.c
@@ -27,7 +27,6 @@ void ether_input(unsigned char *buffer) {
             return;
         case ETHER_TYPE_IP:
             ipv4_input(buffer + ETHER_HEADER_SIZE);
-            printf("%s\n", "IPV4!");
             return;
         case ETHER_TYPE_IPV6:
             printf("%s\n", "IPV6!");
