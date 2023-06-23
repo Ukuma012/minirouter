a1baac2131d543fea00aa941a0486ce7ae798b26
Ukuma012 <Ukuma012@gmail.com>
2023-06-23 20:29:56 +0900

    ip dot notation

--- utils.c
+++ utils.c
@@ -8,3 +8,11 @@ void ip_dot(uint32_t ip) {
     uint8_t d = ip >> 24 & 0x000000ff;
     printf("%d.%d.%d.%d\n", a, b, c, d);
 }
+
+void ipv4_address_set_dot(uint32_t ip) {
+    uint8_t d = ip >> 24 & 0x000000ff;
+    uint8_t c = ip >> 16 & 0x000000ff;
+    uint8_t b = ip >> 8 & 0x000000ff;
+    uint8_t a = ip & 0x000000ff;
+    printf("%d.%d.%d.%d\n", d, c, b, a);
+}
--- utils.h
+++ utils.h
@@ -4,5 +4,6 @@
 #include <stdint.h>
 
 void ip_dot(uint32_t);
+void ipv4_address_set_dot(uint32_t);
 
 #endif //UTILS_H
