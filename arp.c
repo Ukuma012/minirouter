c2d51667887291e8fc2d353e0a1c7b3646fa303a
Ukuma012 <Ukuma012@gmail.com>
2023-06-26 10:24:48 +0900

    arp_cash_add

--- arp.c
+++ arp.c
@@ -1,24 +1,73 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
 #include <arpa/inet.h>
+#include <string.h>
 #include "net.h"
 #include "arp.h"
 #include "utils.h"
 
-struct arp_header {
-   uint16_t hardware_type;
-   uint16_t protocol_type;
-   uint8_t hardware_len;
-   uint8_t protocol_len;
-   uint16_t operation_code; // ARP_request:1, ARP_reply:2
-   uint8_t source_mac_addr[6];
-   uint32_t source_protocol_addr;
-   uint8_t target_mac_addr[6];
-   uint32_t target_protocol_addr;
-} __attribute__ ((__packed__));
+#define ARP_CASH_SIZE 32
 
-void arp_dump(unsigned char *buffer) {
-    struct arp_header* arp_header;
+struct arp_entry arp_cash[ARP_CASH_SIZE];
+
+struct arp_header
+{
+    uint16_t hardware_type;
+    uint16_t protocol_type;
+    uint8_t hardware_len;
+    uint8_t protocol_len;
+    uint16_t operation_code; // ARP_request:1, ARP_reply:2
+    uint8_t source_mac_addr[6];
+    uint32_t source_protocol_addr;
+    uint8_t target_mac_addr[6];
+    uint32_t target_protocol_addr;
+} __attribute__((__packed__));
+
+void arp_cash_add(struct net_device *dev, uint8_t *mac_addr, uint32_t ip_addr)
+{
+    uint32_t index;
+    index = ip_addr % ARP_CASH_SIZE;
+    struct arp_entry *candidate = &arp_cash[index];
+
+    if (candidate->ip_addr == 0 || candidate->ip_addr == ip_addr)
+    {
+        memcpy(candidate->mac_addr, mac_addr, 6);
+        candidate->ip_addr = ip_addr;
+        candidate->dev = dev;
+        return;
+    }
+
+    while (candidate->next != NULL)
+    {
+        candidate = candidate->next;
+
+        if (candidate->ip_addr == ip_addr)
+        {
+            memcpy(candidate->mac_addr, mac_addr, 6);
+            candidate->ip_addr = ip_addr;
+            candidate->dev = dev;
+            return;
+        }
+    }
+
+    struct arp_entry *tmp;
+    if ((tmp = calloc(1, sizeof(struct arp_entry))) == NULL)
+    {
+        fprintf(stderr, "calloc failed\n");
+        exit(1);
+    }
+    candidate->next = tmp;
+    memcpy(tmp->mac_addr, mac_addr, 6);
+    tmp->ip_addr = ip_addr;
+    tmp->dev = dev;
+
+    return;
+}
+
+void arp_dump(unsigned char *buffer)
+{
+    struct arp_header *arp_header;
     arp_header = (struct arp_header *)buffer;
 
     printf("%s\n", "ARP");
@@ -29,7 +78,7 @@ void arp_dump(unsigned char *buffer) {
     printf("operation code: %04X\n", ntohs(arp_header->operation_code));
     printf("Source MAC address: %02X:%02X:%02X:%02X:%02X:%02X\n", arp_header->source_mac_addr[0], arp_header->source_mac_addr[1], arp_header->source_mac_addr[2], arp_header->source_mac_addr[3], arp_header->source_mac_addr[4], arp_header->source_mac_addr[5]);
     printf("Source IP address: ");
-    ip_dot(arp_header->source_protocol_addr); 
+    ip_dot(arp_header->source_protocol_addr);
     printf("Target MAC address: %02X:%02X:%02X:%02X:%02X:%02X\n", arp_header->target_mac_addr[0], arp_header->target_mac_addr[1], arp_header->target_mac_addr[2], arp_header->target_mac_addr[3], arp_header->target_mac_addr[4], arp_header->target_mac_addr[5]);
     printf("Target IP address: ");
     ip_dot(arp_header->target_protocol_addr);
@@ -37,7 +86,8 @@ void arp_dump(unsigned char *buffer) {
     return;
 }
 
-void arp_input(struct net_device *dev, unsigned char *buffer, ssize_t len) {
+void arp_input(struct net_device *dev, unsigned char *buffer, ssize_t len)
+{
     printf("%s\n", "hello");
     return;
 }
--- arp.h
+++ arp.h
@@ -1,6 +1,18 @@
 #ifndef ARP_H
 #define ARP_H
 
+struct net_device;
+
+struct arp_entry
+{
+    uint8_t mac_addr[6];
+    uint32_t ip_addr;
+    struct net_device *dev;
+    struct arp_entry *next;
+};
+
+void arp_cash_add(struct net_device *, uint8_t *, uint32_t ip_addr);
 void arp_dump(unsigned char *);
+void arp_input(struct net_device *, unsigned char *, ssize_t);
 
 #endif // ARP_H
