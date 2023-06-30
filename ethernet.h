197bbc3f72affa30aa7a2b884291084ccbee4d36
Ukuma012 <Ukuma012@gmail.com>
2023-06-30 11:07:32 +0900

    output

--- ethernet.h
+++ ethernet.h
@@ -4,12 +4,15 @@
 struct net_device;
 struct mbuf;
 
+#define ETHER_ADDR_LENGTH 6
 #define ETHER_TYPE_IP 0x0800
 #define ETHER_TYPE_ARP 0x0806
 #define ETHER_TYPE_IPV6 0x86dd
 
+const uint8_t ETHERNET_BROADCAST[] = {0xff, 0xff, 0xff, 0xff, 0xff, 0xff}; 
+
 void ether_input(struct net_device *, unsigned char *, ssize_t);
-void ether_output(struct net_device *, struct mbuf *, uint8_t *, uint16_t);
+void ether_output(struct net_device *, struct mbuf *, const uint8_t *, uint16_t);
 void ether_dump(unsigned char *);
 
 #endif // ETHERNET_H
