4e33ddab4c49ee3fa7f8f73fdd82d41506da055a
Ukuma012 <Ukuma012@gmail.com>
2023-07-02 10:45:15 +0900

    modified

--- net.c
+++ net.c
@@ -3,7 +3,7 @@
 
 struct net_device *dev_base;
 
-struct net_device * get_net_device(char *device_name) {
+struct net_device * net_get_device_name(char *device_name) {
     for(struct net_device *dev = dev_base; dev; dev = dev->next) {
         if((strcmp(dev->name, device_name)) == 0) {
             return dev;
