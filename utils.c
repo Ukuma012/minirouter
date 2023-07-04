ce4bf97464cabea708fe9a5657bf9bf8abae1392
Ukuma012 <Ukuma012@gmail.com>
2023-07-04 10:08:09 +0900

    checksum function

--- utils.c
+++ utils.c
@@ -16,3 +16,23 @@ void ipv4_address_set_dot(uint32_t ip) {
     uint8_t a = ip & 0x000000ff;
     printf("%d.%d.%d.%d\n", d, c, b, a);
 }
+
+uint16_t calculate_checksum(const void *data, size_t len) {
+    const uint16_t *buffer = (const uint16_t *)data;
+    uint32_t sum = 0;
+
+    while(len > 1) {
+        sum += *buffer++;
+        len -= 2;
+    }
+
+    if(len > 0) {
+        sum += *(const uint8_t *)buffer;
+    }
+
+    while(sum >> 16) {
+        sum = (sum & 0xffff) + (sum >> 16);
+    }
+
+    return (uint16_t)(~sum);
+}
--- utils.h
+++ utils.h
@@ -5,5 +5,6 @@
 
 void ip_dot(uint32_t);
 void ipv4_address_set_dot(uint32_t);
+uint16_t calculate_checksum(const void *, size_t);
 
 #endif //UTILS_H
