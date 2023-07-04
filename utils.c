a987f4cde97fbeaaa8959cbd17effcc947bbccdd
Ukuma012 <Ukuma012@gmail.com>
2023-07-04 22:33:09 +0900

    resolve the confusion

--- utils.c
+++ utils.c
@@ -1,20 +1,12 @@
 #include <stdint.h>
 #include <stdio.h>
 
-void ip_dot(uint32_t ip) {
+void ipv4_ntoh_dot(uint32_t ip) {
     uint8_t a = ip & 0x000000ff;
     uint8_t b = ip >> 8 & 0x000000ff;
     uint8_t c = ip >> 16 & 0x000000ff;
     uint8_t d = ip >> 24 & 0x000000ff;
     printf("%d.%d.%d.%d\n", a, b, c, d);
-}
-
-void ipv4_address_set_dot(uint32_t ip) {
-    uint8_t d = ip >> 24 & 0x000000ff;
-    uint8_t c = ip >> 16 & 0x000000ff;
-    uint8_t b = ip >> 8 & 0x000000ff;
-    uint8_t a = ip & 0x000000ff;
-    printf("%d.%d.%d.%d\n", d, c, b, a);
 }
 
 uint16_t calculate_checksum(const void *data, size_t len) {
--- utils.h
+++ utils.h
@@ -3,8 +3,7 @@
 
 #include <stdint.h>
 
-void ip_dot(uint32_t);
-void ipv4_address_set_dot(uint32_t);
+void ipv4_ntoh_dot(uint32_t);
 uint16_t calculate_checksum(const void *, size_t);
 
 #endif //UTILS_H
