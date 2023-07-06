215b89e28253f5fd23bd68bf5c5261932c455e3b
Ukuma012 <Ukuma012@gmail.com>
2023-07-06 10:56:12 +0900

    modified

--- ip.c
+++ ip.c
@@ -12,20 +12,6 @@
 #include "mbuf.h"
 #include "icmp.h"
 
-struct ipv4_header
-{
-    uint8_t version_ihl;
-    uint8_t type_of_service;
-    uint16_t total_length;
-    uint16_t identification;
-    uint16_t flags_fragment_offset;
-    uint8_t TTL;
-    uint8_t protocol;
-    uint16_t header_checksum;
-    uint32_t source_ipv4_addr;
-    uint32_t destination_ipv4_addr;
-} __attribute__((__packed__));
-
 void ipv4_dump(unsigned char *buffer)
 {
     struct ipv4_header *ipv4_header;
--- ip.h
+++ ip.h
@@ -15,6 +15,20 @@ struct ipv4_device
     uint32_t broadcast_addr;
 };
 
+struct ipv4_header
+{
+    uint8_t version_ihl;
+    uint8_t type_of_service;
+    uint16_t total_length;
+    uint16_t identification;
+    uint16_t flags_fragment_offset;
+    uint8_t TTL;
+    uint8_t protocol;
+    uint16_t header_checksum;
+    uint32_t source_ipv4_addr;
+    uint32_t destination_ipv4_addr;
+} __attribute__((__packed__));
+
 struct net_device;
 struct mbuf;
 
