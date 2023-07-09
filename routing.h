5d1e0459bb311889fe400859b5088ed21d637452
Ukuma012 <Ukuma012@gmail.com>
2023-07-09 15:24:30 +0900

    data structure

--- routing.h
+++ routing.h
@@ -2,13 +2,23 @@
 #define ROUTING_H
 
 #include <stdint.h>
+#include "net.h"
+
+enum routing_type {
+    directly_connected,
+    network
+};
+
+struct routing_entry {
+    enum routing_type type;
+    union{
+        struct net_device *dev;
+        uint32_t next_hop;
+    };
+};
 
 struct routing_trie_node {
-    int is_leaf;
-    char dev_name[32];
-    uint32_t transfer_ip_addr;
-    bool is_directly_connected;
-    bool is_next_hop;
+    struct routing_entry *data;
     struct routing_trie_node *node_zero;
     struct routing_trie_node *node_one;
 };
